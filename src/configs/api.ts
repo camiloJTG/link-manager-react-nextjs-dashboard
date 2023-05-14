0ca87da86eae1fe5d46e3d44db2e576f94066171
camiloJTG <camilojtg.devep@protonmail.com>
2023-05-14 14:26:09 -0400

    feat: Add config module

---
+++ src/configs/api.ts
@@ -0,0 +1,12 @@
+const API = process.env.NEXT_PUBLIC_LINK_API_URL_BASE;
+
+export const apiConfig = {
+   auth: {
+      register: `${API}/user/register`,
+      login: `${API}/auth/login`
+   },
+   link: {
+      create: `${API}/link`,
+      listLink: `${API}/link/user`
+   }
+};
